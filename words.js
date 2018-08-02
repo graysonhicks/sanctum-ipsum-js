1b5859424aeb094fcf2bb5067744003140b51431
Grayson Hicks <graysonhicks@gmail.com>
2018-08-01 20:15:33 -0400

    update words to new translation

--- words.js
+++ words.js
@@ -1,11 +1,10 @@
-module.exports = ["pope", "catholic", "vatican", "transubstantiation", "universal", "communion", "monk", "monastery", "priest", "nun", "abbot", "cathedral", "holy Mary, Mother of God",
-        "our Father, who art in Heaven",
-        "the sacred heart of Jesus", "peace be with you, and also with you", "St. Michael the Archangel defend us in battle", "salve regina", "St. Thomas Aquinas", "Thomas Merton", "Fatima",
-        "Lourdes", "eucharist", "Trappist", "Ursuline", "Cistercian", "liturgy of the hours", "confiteor", "dogma", "tradition", "Papal enclave", "feast day", "saint", "tridentine",
-        "Benedictine", "canonization", "beatified", "St. Rose of Lima", "St. Gabriel", "John-Henry Newman", "annunciation", "assumption", "Kyrie eleison", "Christe eleison", "pontificate",
-        "vicar of Christ", "intercession", "Paschal mystery", "Apostles creed", "Nicene creed", "St. Francis of Assisi", "Pope St. John Paul II", "Pope Francis", "rosary", "relic",
-        "saint medal", "stations of the cross", "immaculate conception", "triduum", "Holy days of obligation", "opus Dei", "annointing of the sick", "adoration", "Ash Wednesday", "confession",
-        "miter", "ecumenical council", "catechesis", "natural law", "monsignor", "diocese", "seal of confession", "consecration", "encyclical", "deacon", "apostolic succession", "litany",
-        "magisterium", "College of Cardinals", "Padre Pio", "St. Therese of Lisieux", "Our Lady of Guadalupe", "bless us, oh lord, and these thy gifts", "forgive me Father, for I have sinned",
-        "St. Joseph", "St. James", "novena"
-    ]
+module.exports=["pope","catholic","vatican","transubstantiation","universal","communion","monk","monastery","priest","nun","abbot","cathedral","holyMary,MotherofGod",
+"ourFather,whoartinHeaven",
+"thesacredheartofJesus","peacebewithyou,andalsowithyou","St.MichaeltheArchangeldefendusinbattle","salveregina","St.ThomasAquinas","ThomasMerton","Fatima",
+"Lourdes","eucharist","Trappist","Ursuline","Cistercian","liturgyofthehours","confiteor","dogma","tradition","Papalenclave","feastday","saint","tridentine",
+"Benedictine","canonization","beatified","St.RoseofLima","St.Gabriel","John-HenryNewman","annunciation","assumption","Kyrieeleison","Christeeleison","pontificate",
+"vicarofChrist","intercession","Paschalmystery","Apostlescreed","Nicenecreed","St.FrancisofAssisi","PopeSt.JohnPaulII","PopeFrancis","rosary","relic",
+"saintmedal","stationsofthecross","immaculateconception","triduum","Holydaysofobligation","opusDei","annointingofthesick","adoration","AshWednesday","confession",
+"miter","ecumenicalcouncil","catechesis","naturallaw","monsignor","diocese","sealofconfession","consecration","encyclical","deacon","apostolicsuccession","litany",
+"magisterium","CollegeofCardinals","PadrePio","St.ThereseofLisieux","OurLadyofGuadalupe","blessus,ohlord,andthesethygifts","forgivemeFather,forIhavesinned",
+"St.Joseph","St.James","novena"]
